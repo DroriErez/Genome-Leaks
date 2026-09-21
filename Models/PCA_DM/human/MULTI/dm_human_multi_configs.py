0218ac6026089e3f276f62626f195f2f7170426a
DroriErez <dror.erez.1969@gmail.com>
2026-09-21 21:40:31 +0300

    change ac_gan configurations

--- Models/PCA_DM/human/MULTI/dm_human_multi_configs.py
+++ Models/PCA_DM/human/MULTI/dm_human_multi_configs.py
@@ -18,8 +18,8 @@ CONFIG_DM_HUMAN_MULTI["hidden_dim_3"] = 16384
 CONFIG_DM_HUMAN_MULTI["batch_size"] = 8096
 CONFIG_DM_HUMAN_MULTI["optim_lr"] = 3e-4
 CONFIG_DM_HUMAN_MULTI["optim_lr_min"] = 1e-6 # 3e-12 have been used in some diffusion model implementations.  
-CONFIG_DM_HUMAN_MULTI["num_epochs"] = 20000       
-CONFIG_DM_HUMAN_MULTI["checkpoint"] = 200
+CONFIG_DM_HUMAN_MULTI["num_epochs"] = 200
+CONFIG_DM_HUMAN_MULTI["checkpoint"] = 20
 CONFIG_DM_HUMAN_MULTI["warmup_period"] = 1000   
 
 # Device
